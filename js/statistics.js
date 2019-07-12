bdd4a37dc254c67703b7032fe01fb79bca0aca44
Svante Geisshirt <svante2001@gmail.com>
2019-07-12 22:00:14 +0200

    WIP

--- js/statistics.js
+++ js/statistics.js
@@ -1,23 +1,29 @@
 function statistics() {
-    const input = document.getElementById('statArrayInput');
-    input.onsubmit = fucntion(); {
-        var numbers = new Array(input)
-        // document.getElementById('statArrayInput').value = '';
+    var numbers = [];
+    document.getElementById('statAdd').onclick = function() {
+        const input = document.getElementById('statArrayInput');
+        numbers.push(parseFloat(input.value));
+        document.getElementById('statArrayInput').value = '';
+    }
 
-        document.getElementById('statCalc').onclick = function () {
+    document.getElementById('statCalc').onclick = function() {
+        // Sum and average
+        var sum = 0;
+        for (var i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        document.getElementById('Sum').innerHTML = 'Sum: ' + sum;
+        var avg = sum / numbers.length;
+        document.getElementById('Gennemsnit').innerHTML = 'Gennemsnit: ' + avg;
 
-            // Sum and average
-            var sum = 0;
-            for( var i = 0; i < numbers.length; i++ ){
-                sum++
-            }
-            var avg = sum/numbers.length;
-            document.getElementById('Sum').innerHTML = 'Sum: ' + sum;
-            document.getElementById('Gennemsnit').innerHTML = 'Gennemsnit: ' + avg;
+        // Bubble sort to make the dataset in order to find the highest number
+        var sortedNumbers = numbers.sort()
 
-            // Q1, median and Q3
-
-            // bubble sort to find highest number
+        // Q1, median and Q3
+        if (numbers.length % 2 !== 0) {
+            document.getElementById('Median').innerHTML = 'Median: ' + numbers[(numbers.length + 1) / 2];
+        } else {
+            document.getElementById('Median').innerHTML = 'Median: ' + numbers[numbers.length / 2];
         }
     }
 }
